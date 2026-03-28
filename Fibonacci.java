d4fda04570097cf8334683d2ecf4f4e65a6cc1c3
corebyte56 <fahim789m@gmail.com>
2026-03-28 21:21:30 +0600

    Add Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, t1 = 0, t2 = 1;
+        System.out.print("Fibonacci: " + t1 + " " + t2 + " ");
+        for(int i = 3; i <= n; i++){
+            int next = t1 + t2;
+            System.out.print(next + " ");
+            t1 = t2;
+            t2 = next;
+        }
+    }
+}
